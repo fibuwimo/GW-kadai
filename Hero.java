1b3c610f45713504461656b7d57c68dfbebcdbcc
fibuwimo <ray-rem-craim-requiem@ezweb.ne.jp>
2022-05-10 13:11:26 +0900

    GW-kadai

--- Hero.java
+++ Hero.java
@@ -23,19 +23,20 @@ public class Hero extends Character{
 		"(詠唱:"+magicGages[5]+",継続回復↑バフ)",
 		"",
 	};
-	int magicNum =0;
-	int magicGage=0;
-	int agiAdd=0;
-	int agiAddCount=0;
-	int spellAdd=0;
-	int spellAddCount=0;
-	int hotAdd=0;
-	int hotAddCount=0;
+	int magicNum =0; //どの魔法が詠唱中か（０は詠唱中でない）
+	int magicGage=0; //詠唱されている魔法の詠唱必要値
+	int agiAdd=0; //行動加速
+	int agiAddCount=0; //行動加速残りターン
+	int spellAdd=0; //詠唱加速
+	int spellAddCount=0; //詠唱加速残りターン
+	int hotAdd=0; //毎ターンごとのリジェネ値
+	int hotAddCount=0; //リジェネ残りターン
 	Hero(String name,int hp,int atk,int agi){
 		super(name,hp,atk,agi);
 	}
-	boolean actGage(){
-		if(this.magicNum!=0){
+
+	boolean actGage(){ //ゲージ表記とゲージ判定
+		if(this.magicNum!=0){//魔法詠唱中なら
 			this.count +=1+this.spellAdd;
 			System.out.print(this.name+"HP:"+this.hp+"【spell:");
 			for(int i=0;i<magicGage;i++){
@@ -46,13 +47,14 @@ public class Hero extends Character{
 			System.out.print(spellAdd==1?"詠":"");
 			System.out.print(hotAdd>0?"リ":"");
 			System.out.println(")】");
-			if(this.count>=magicGage){
+			if(this.count>=magicGage){　//詠唱カウントが魔法の詠唱必要値に達したなら
 				this.count=0;
 				return true;
 			}else{
 				return false;
 			}
 		}
+		//魔法詠唱中でないなら
 		this.count +=this.agi+this.agiAdd;
 		System.out.print(this.name+"HP:"+this.hp+"【act:");
 		for(int i=0;i<GAGE;i++){
@@ -63,7 +65,7 @@ public class Hero extends Character{
 		System.out.print(spellAdd==1?"詠":"");
 		System.out.print(hotAdd>0?"リ":"");
 		System.out.println(")】");
-		if(this.count>=GAGE){
+		if(this.count>=GAGE){ //行動カウントがGAGE（今回は１２）に達したなら
 			this.count=0;
 			return true;
 		}else{
@@ -71,13 +73,13 @@ public class Hero extends Character{
 		}
 	}
 
-	void act(Character c){
-		this.buffCount();
-		this.act1(c);
+	void act(Character c){　//ゲージ満了後にAppから呼ばれる
+		this.buffCount();　//バフ等ターンごとの処理
+		this.act1(c);　//下記のact処理を呼ぶ
 	}
 	void act1(Character c){
-		if(this.magicNum!=0){
-			switch(this.magicNum){
+		if(this.magicNum!=0){ //魔法詠唱中であったなら
+			switch(this.magicNum){　//詠唱中の魔法によってスイッチ
 				case 1:
 					this.fire(c);
 					break;
@@ -97,14 +99,14 @@ public class Hero extends Character{
 					this.regen(c);
 					break;
 			}
-			this.magicNum=0;
+			this.magicNum=0;　//魔法詠唱状態解除
 			return;
 		}
-
+		//魔法詠唱中でなかったなら
 		for(int i=0;i<acts.length;i++){
 			System.out.printf("%d:%s%n",i,acts[i]);
 		}
-		int idx;
+		int idx;　//行動選択
 		do{
 			System.out.printf("0~%d>>",acts.length-1);
 			idx = new Scanner(System.in).nextInt();
@@ -118,7 +120,8 @@ public class Hero extends Character{
 				break;
 		}
 	}
-	void magic(Character c){
+
+	void magic(Character c){ //魔法選択
 		for(int i=0;i<magics.length;i++){
 			System.out.printf("%d:%s  %s%n",i,magics[i],magicsinfo[i]);
 		}
@@ -128,13 +131,15 @@ public class Hero extends Character{
 			idx = new Scanner(System.in).nextInt();
 		}while(idx<0 || idx>=magics.length);
 		if(idx==magics.length-1){
-			this.act1(c);
+			this.act1(c); //戻るが選択されたらact1を再度呼ぶ
 			return;
 		}
 		System.out.printf("%sは%sの詠唱を開始した%n",this.name,this.magics[idx]);
-		this.magicNum=idx+1;
-		this.magicGage=this.magicGages[idx];
+		this.magicNum=idx+1;　//どの魔法の詠唱を開始したかをセット
+		this.magicGage=this.magicGages[idx];　//魔法の詠唱必要値をセット
 	}
+
+	//以下魔法リスト
 
 	void fire(Character c){
 		System.out.printf("%sの%s！%sに%dのダメージを与えた！%n",this.name,this.magics[0],c.name,this.atk*3);
@@ -163,6 +168,9 @@ public class Hero extends Character{
 		this.hotAdd=10;
 		this.hotAddCount=10;
 	}
+	
+	//以下バフカウント（actが呼ばれるごとに起動）
+
 	void buffCount(){
 		if(agiAddCount>0) agiAddCount--;
 		if(agiAdd==1 && agiAddCount==0){
